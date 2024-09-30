cc1bbd5600bf2a908e8fdfc13723f020047229fc
MahammadAtheekSkaik <129764770+ShaikMahammadAtheek@users.noreply.github.com>
2024-10-01 02:14:01 +0530

    Update OffCampus.js

--- client/src/pages/OffCampus.js
+++ client/src/pages/OffCampus.js
@@ -71,7 +71,7 @@ const OffCampus = () => {
   const [jobs, setJobs] = useState([]);
 
   useEffect(() => {
-    axios.get('https://jobs-hustle.onrender.com/api/offcampus')  // Make sure the API URL is correct
+    axios.get('https://jobshustle-tobl.onrender.com/api/offcampus')  // Make sure the API URL is correct
       .then((response) => {
         setJobs(response.data);  // Update jobs with response data
       })
