22c9e3a11a5663c5bef2d72ee201c38999325974
MahammadAtheekSkaik <129764770+ShaikMahammadAtheek@users.noreply.github.com>
2024-10-02 01:54:56 +0530

    Update Home.js

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -24,7 +24,7 @@ const Home = () => {
     const [jobs, setJobs] = useState([]);
 
     useEffect(() => {
-        axios.get('https://jobshustle-tobl.onrender.com/api/home') // Fetch sorted jobs
+        axios.get('https://jobs-hustle.onrender.com/api/home') // Fetch sorted jobs
             .then((response) => {
                 setJobs(response.data); // Set the jobs in state
             })
